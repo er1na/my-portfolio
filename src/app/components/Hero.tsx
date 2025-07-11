f66d5f6b60282e7fb6733ce7d2633780fe8b8079
er1na <ern20501prg@gmail.com>
2025-07-11 22:44:54 +0900

    first commit

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,16 @@
+export default function Hero() {
+    return (
+        <div>            
+            <div>
+                PORTFORIO
+            </div>
+            <div>
+                2025
+            </div>
+            <div>
+                SITE
+            </div>
+        </div>
+    );
+
+}
---
+++ src/app/parts/menu.tsx
@@ -0,0 +1,13 @@
+export default function Menu() {
+    return (
+        <div>
+            <ul>
+                <li>ABOUT</li>
+                <li>BLOG</li>
+                <li>SKILLS</li>
+                <li>CONTACT</li>
+                <li>RESUME</li>
+            </ul>
+        </div>
+    );
+}
