a08be82153a7c293f5b2738c9dfbcf9f909a23c2
er1na <ern20501prg@gmail.com>
2025-07-16 23:59:12 +0900

    aboutコンポーネント追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="overflow-x-hidden">
       <Hero />
+      <About />
     </div>
   );
 }
